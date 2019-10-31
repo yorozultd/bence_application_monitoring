0c30caa1450d5636b80c3de5badafae598000bf8
Ch34TC0D3 <ch34tc0d3@pop-os.localdomain>
2019-10-31 14:09:44 +0530

    Working

--- send_monitoring_email.py
+++ send_monitoring_email.py
@@ -3,25 +3,52 @@ import xml.etree.ElementTree as ET
 import requests
 import numpy 
 import smtplib 
-'''
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from datetime import date
+today= date.today()
+d4 = today.strftime("%b-%d-%Y")
+
+
+fromaddr = "hikari.code@gmail.com"
+toaddr = "hikari.code@gmail.com"
+message = MIMEMultipart("alternative")
+message["Subject"] = str(d4) + " Daily Report"
+message["From"] = fromaddr
+message["To"] = toaddr
+
+
+
+
+html = "<ol>"
 r=requests.get("https://ldczk.com/state_endpoint/")
-et= ET.parse(r.content)
-root= ET.getroot();
-root.findall('application')
-'''
+with open('./applications.xml','wb') as f1 : 
+    f1.write(r.content)
+et= ET.parse('./applications.xml')
+root= et.getroot();
+applications = root.findall('application')
+
+for application in applications  : 
+    html+= "<h1><li>"+application.find('application_name').text+ "</li></h1><ul>"
+    html+= "<pre><li><h3 style='display:inline;'><b><u>Application Status</u>  : </b></h3><div  style='display:inline;'>"+application.find('application_status').text+ "</div></li></pre>"
+    html+= "<pre><li><h3  style='display:inline;' ><b><u>Comment</u>  : </b></h3><div  style='display:inline;'>"+application.find('comment').text+ "</div></li></pre>"
+    html+= "<pre><li><h3  style='display:inline;' ><b><u>Update Date</u>  : </b></h3><div  style='display:inline;'>"+application.find('update_date').text+ "</div></pre></li></ul>"
+
+html+='</ol>'
 
 #s = smtplib.SMTP('smtp.gmail.com', 587) 
-s = smtplib.SMTP('aspmx.l.google.com', 25) 
+s = smtplib.SMTP('smtp.elasticemail.com', 2525) 
 
 s.ehlo()
 s.starttls() 
 s.ehlo()
 #s.login('cooperatepvtinc@gmail.com','c00p3r4t3') 
-#s.login('cooperatepvtinc@gmail.com','bb65194c-4aea-4643-b4e9-15bd76038a30') 
+s.login('hikari.code@gmail.com','eedd6938-3151-4955-97e1-7c7a8c6f89b2') 
 
-fromaddr = "cooperatepvtinc@gmail.com"
-toaddr = "faltushiv9@gmail.com"
-message = "Subject: Information Regarding Mail \n\n Mail is working to unsubscribe please wait"
-s.sendmail(fromaddr, toaddr, message) 
+part2 = MIMEText(html, "html")
+
+message.attach(part2)
+s.sendmail(fromaddr, toaddr, message.as_string()) 
   
 s.quit() 
+    
